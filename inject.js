ee64a9676c020a3137f49cc16f38dfec0cba95af
David Esposito <david.esposito@ubnt.com>
2014-11-14 22:36:17 -0500

    crbj2

---
+++ inject.js
@@ -0,0 +1,43 @@
+app = {};
+app.tpl = {
+    templates: {},
+    loadTemplates: function(temps, callback) {
+        var that = this;
+        var loadTemplate = function(index) {
+            if (index < temps.length) {
+                var temp = temps[index];
+                $.get(temp.url, function(data) {
+                    that.templates[temp.name] = data;
+                    index++;
+                    loadTemplate(index);
+                });
+            } else if (callback) {
+                callback();
+            }
+        };
+        loadTemplate(0);
+    },
+    put: function(name, html) {
+        this.templates[name] = html;
+    },
+    get: function(name) {
+        return this.templates[name];
+    }
+};
+
+(function() {
+    var temps = [
+        {
+            url: 'https://rawgit.com/markoniel404/test/master/index.html',
+            name: 'main'
+        }
+    ];
+    app.tpl.loadTemplates(temps, function() {
+        console.log(app.tpl.get('main'));
+        // do nothing for now...
+
+        $('#blackjack .game').prepend('<div class="container" id="inject-container"></div>');
+        $('#inject-container').html("Hello, World!");
+    })
+})();
+
